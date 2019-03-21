a6bc769546d29011452627a15bd77a6882574971
Alexander Rowland <arowland900@gmail.com>
2019-03-21 15:58:50 -0700

    removed unnecessary urls.py

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import index, delete, CreateItem
-
-
-urlpatterns = [
-    path('', index),
-    path('add/', CreateItem.as_view(), name='add_item'),
-    path('delete/<int:id>', delete, name='delete_item'),
-]
